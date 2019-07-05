870a05cada9a3082b02b13362ba6ef8a772f3a36
Ali Sabil <ali.sabil@koperadev.com>
2019-07-06 00:29:52 +0200

    board: add imx8mm_kopera_polar0

---
+++ board/kopera/imx8mm_kopera_polar0/imx8mm_kopera_polar0.c
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2018 NXP
+ * Copyright 2019 Variscite Ltd.
+ * Copyright 2019 KOPERA
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <asm/io.h>
+#include <miiphy.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <asm-generic/gpio.h>
+#include <asm/arch/imx8mm_pins.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/mach-imx/gpio.h>
+#include <asm/mach-imx/mxc_i2c.h>
+#include <asm/arch/clock.h>
+#include <usb.h>
+
+#include "../common/variscite/imx8m_eeprom.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int board_early_init_f(void)
+{
+	return 0;
+}
+
+#ifdef CONFIG_BOARD_POSTCLK_INIT
+int board_postclk_init(void)
+{
+	/* TODO */
+	return 0;
+}
+#endif
+
+/* Return DRAM size in bytes */
+static unsigned long long get_dram_size(void)
+{
+	int ret;
+	u32 dram_size_mb;
+	struct var_eeprom eeprom;
+
+	var_eeprom_read_header(&eeprom);
+	ret = var_eeprom_get_dram_size(&eeprom, &dram_size_mb);
+	if (ret)
+		return (1ULL * DEFAULT_DRAM_SIZE_MB ) << 20;
+
+	return (1ULL * dram_size_mb) << 20;
+}
+
+int dram_init_banksize(void)
+{
+	gd->bd->bi_dram[0].start = CONFIG_SYS_SDRAM_BASE;
+	gd->bd->bi_dram[0].size = get_dram_size();
+
+	return 0;
+}
+
+int dram_init(void)
+{
+	unsigned long long mem_size, max_low_size, dram_size;
+
+	max_low_size = 0x100000000ULL - CONFIG_SYS_SDRAM_BASE;
+	dram_size = get_dram_size();
+	
+	if (dram_size > max_low_size)
+		mem_size = max_low_size;
+	else
+		mem_size = dram_size;
+
+	/* rom_pointer[1] contains the size of TEE occupies */
+	gd->ram_size = mem_size - rom_pointer[1];
+
+	return 0;
+}
+
+#ifdef CONFIG_OF_BOARD_SETUP
+int ft_board_setup(void *blob, bd_t *bd)
+{
+	return 0;
+}
+#endif
+
+#define VOLUME_UP_KEY		IMX_GPIO_NR(3, 25)
+#define VOLUME_UP_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_HYS | PAD_CTL_PUE)
+
+static iomux_v3_cfg_t const volume_up_pads[] = {
+	IMX8MQ_PAD_SAI5_RXD3__GPIO3_IO24 | MUX_PAD_CTRL(VOLUME_UP_PAD_CTRL),
+};
+
+static int board_gpio_init(void)
+{
+	/* We are only interested in the '+' key i.e. volume up key.
+		We use it as a signal to check for recovery mode
+	*/
+	imx_iomux_v3_setup_multiple_pads(volume_up_pads, ARRAY_SIZE(volume_up_pads));
+	gpio_request(VOLUME_UP_KEY, "VOLUME_UP");
+	gpio_direction_input(VOLUME_UP_KEY);
+
+	return 0;
+}
+
+#ifdef CONFIG_CI_UDC
+int board_usb_init(int index, enum usb_init_type init)
+{
+	imx8m_usb_power(index, true);
+
+	return 0;
+}
+
+int board_usb_cleanup(int index, enum usb_init_type init)
+{
+	imx8m_usb_power(index, false);
+
+	return 0;
+}
+#endif
+
+int board_init(void)
+{
+	board_gpio_init();
+
+	return 0;
+}
+
+int board_mmc_get_env_dev(int devno)
+{
+	return devno - 1;
+}
+
+int mmc_map_to_kernel_blk(int devno)
+{
+	return devno + 1;
+}
+
+int board_late_init(void)
+{
+	env_set("board_name", "imx8mm-kopera-polar0");
+	env_set("board_rev", "0");
+
+#ifdef CONFIG_ENV_IS_IN_MMC
+	board_late_mmc_env_init();
+#endif
+
+	if (gpio_get_value(VOLUME_UP_KEY) == 1) {
+		printf("Recovery mode enabled\n");
+		env_set("bootcmd", "run mmcrecoverycmd");
+	} else {
+		env_set("bootcmd", "run mmcbootcmd");
+	}
+
+	return 0;
+}
---
+++ include/configs/imx8mm_kopera_polar0.h
@@ -0,0 +1,271 @@
+/*
+ * Copyright 2017 NXP
+ * Copyright 2018-2019 Variscite Ltd.
+ * Copyright 2019 KOPERA
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __IMX8MM_KOPERA_POLAR0_H
+#define __IMX8MM_KOPERA_POLAR0_H
+
+#include <linux/sizes.h>
+#include <asm/arch/imx-regs.h>
+
+#ifdef CONFIG_SECURE_BOOT
+#define CONFIG_CSF_SIZE			0x2000 /* 8K region */
+#endif /* CONFIG_SECURE_BOOT */
+
+#define CONFIG_SPL_TEXT_BASE		0x7E1000
+#define CONFIG_SPL_MAX_SIZE		(148 * 1024)
+
+#define CONFIG_SYS_MONITOR_LEN		(512 * 1024)
+#define CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_USE_SECTOR
+#define CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR	0x300
+#define CONFIG_SYS_MMCSD_FS_BOOT_PARTITION	1
+
+#define CONFIG_SYS_UBOOT_BASE		(QSPI0_AMBA_BASE + CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR * 512)
+
+#ifdef CONFIG_SPL_BUILD
+#define CONFIG_SPL_WATCHDOG_SUPPORT
+#define CONFIG_SPL_POWER_SUPPORT
+#define CONFIG_SPL_DRIVERS_MISC_SUPPORT
+#define CONFIG_SPL_I2C_SUPPORT
+#define CONFIG_SPL_LDSCRIPT		"arch/arm/cpu/armv8/u-boot-spl.lds"
+#define CONFIG_SPL_STACK		0x91fff0
+#define CONFIG_SPL_LIBCOMMON_SUPPORT
+#define CONFIG_SPL_LIBGENERIC_SUPPORT
+#define CONFIG_SPL_SERIAL_SUPPORT
+#define CONFIG_SPL_GPIO_SUPPORT
+#define CONFIG_SPL_BSS_START_ADDR      0x00910000
+#define CONFIG_SPL_BSS_MAX_SIZE        0x2000	/* 8 KB */
+#define CONFIG_SYS_SPL_MALLOC_START    0x42200000
+#define CONFIG_SYS_SPL_MALLOC_SIZE     0x80000	/* 512 KB */
+#define CONFIG_SYS_ICACHE_OFF
+#define CONFIG_SYS_DCACHE_OFF
+
+#define CONFIG_MALLOC_F_ADDR		0x912000 /* malloc f used before GD_FLG_FULL_MALLOC_INIT set */
+
+#define CONFIG_SPL_ABORT_ON_RAW_IMAGE /* For RAW image gives a error info not panic */
+
+#undef CONFIG_DM_MMC
+
+#define CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
+
+#define CONFIG_POWER
+#define CONFIG_POWER_I2C
+#define CONFIG_POWER_BD71837
+#endif /* CONFIG_SPL_BUILD */
+
+#define CONFIG_CMD_READ
+#define CONFIG_SERIAL_TAG
+#define CONFIG_FASTBOOT_USB_DEV 0
+
+#define CONFIG_REMAKE_ELF
+
+#define CONFIG_BOARD_EARLY_INIT_F
+#define CONFIG_BOARD_POSTCLK_INIT
+#define CONFIG_BOARD_LATE_INIT
+
+/* Flat Device Tree Definitions */
+#define CONFIG_OF_BOARD_SETUP
+
+#undef CONFIG_CMD_EXPORTENV
+#undef CONFIG_CMD_IMPORTENV
+#undef CONFIG_CMD_IMLS
+
+#undef CONFIG_CMD_CRC32
+#undef CONFIG_BOOTM_NETBSD
+
+/* ENET Config */
+/* ENET1 */
+#if defined(CONFIG_CMD_NET)
+#define CONFIG_CMD_PING
+#define CONFIG_CMD_DHCP
+#define CONFIG_CMD_MII
+#define CONFIG_MII
+#define CONFIG_ETHPRIME                 "FEC"
+
+#define CONFIG_FEC_MXC
+#define CONFIG_FEC_XCV_TYPE             RGMII
+#define CONFIG_FEC_MXC_PHYADDR          0
+#define FEC_QUIRK_ENET_MAC
+
+#define CONFIG_PHY_GIGE
+#define IMX_FEC_BASE			0x30BE0000
+
+#define CONFIG_PHYLIB
+#define CONFIG_PHY_ATHEROS
+#endif
+
+/* Initial environment variables */
+#define CONFIG_EXTRA_ENV_SETTINGS		\
+	"bootdir=/boot\0" \
+	"image=Image.gz\0" \
+	"fdt_file=" CONFIG_DEFAULT_FDT_FILE "\0" \
+	"console=ttymxc0,115200 earlycon=ec_imx6q,0x30860000,115200\0" \
+	\
+	"img_addr=0x42000000\0" \
+	"fdt_addr=0x43000000\0" \
+	"fdt_high=0xffffffffffffffff\0" \
+	"initrd_addr=0x43800000\0" \
+	"initrd_high=0xffffffffffffffff\0" \
+	\
+	"BOOT_ORDER=A B\0" \
+	"BOOT_A_LEFT=3\0" \
+	"BOOT_B_LEFT=3\0" \
+	\
+	"mmcdev="__stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
+	"mmcrootdev="__stringify(CONFIG_SYS_MMC_ENV_DEV + 1)"\0" \
+	"mmcpart=" __stringify(CONFIG_SYS_MMC_IMG_LOAD_PART) "\0" \
+	"mmcroot=" CONFIG_MMCROOT "\0" \
+	"mmcloadimagecmd=load mmc ${mmcdev}:${mmcpart} ${img_addr} ${bootdir}/${image}; " \
+		"unzip ${img_addr} ${loadaddr};\0" \
+	"mmcloadfdtcmd=load mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${bootdir}/${fdt_file};\0" \
+	"mmcselectpartcmd=setenv mmcpart; " \
+		"for BOOT_SLOT in \"${BOOT_ORDER}\"; do " \
+			"if test \"x${mmcpart}\" = \"x\"; then " \
+				"if test \"x${BOOT_SLOT}\" = \"xA\"; then " \
+					"if test ${BOOT_A_LEFT} -gt 0; then " \
+						"setexpr BOOT_A_LEFT ${BOOT_A_LEFT} - 1; " \
+						"echo \"Found valid slot A, ${BOOT_A_LEFT} attempts remaining\"; " \
+						"setenv mmcpart 1; " \
+					"fi; " \
+				"elif test \"x${BOOT_SLOT}\" = \"xB\"; then " \
+					"if test ${BOOT_B_LEFT} -gt 0; then " \
+						"setexpr BOOT_B_LEFT ${BOOT_B_LEFT} - 1; " \
+						"echo \"Found valid slot B, ${BOOT_B_LEFT} attempts remaining\"; " \
+						"setenv mmcpart 2; " \
+					"fi; " \
+				"fi; " \
+			"fi; " \
+		"done; " \
+		"if test -n \"${mmcpart}\"; then " \
+			"setenv mmcroot /dev/mmcblk${mmcrootdev}p${mmcpart}; " \
+			"saveenv; " \
+		"else " \
+			"echo \"No valid slot found, resetting tries to 3\"; " \
+			"setenv BOOT_A_LEFT 3; " \
+			"setenv BOOT_B_LEFT 3; " \
+			"saveenv; " \
+			"reset; " \
+		"fi;\0" \
+	"mmcbootcmd=if run mmcselectpartcmd && run mmcloadimagecmd && run mmcloadfdtcmd; then " \
+		"setenv bootargs console=${console} root=${mmcroot} rootwait vt.global_cursor_default=0 panic=-1; " \
+		"booti ${loadaddr} - ${fdt_addr}; " \
+	"fi;\0" \
+	"mmcrecoverycmd=echo \"Initialising recovery\"; " \
+		"if usb reset && load usb 0 ${loadaddr} polarfw.bin; then " \
+			"echo \"Firmware image size : 0x${filesize} bytes.\"; " \
+			"echo \"Flashing image to mmc 0\"; " \
+			"echo \"DO NOT REMOVE POWER OR RESET UNTIL THIS PROCESS IS FINISHED\"; " \
+			"gzwrite mmc 0 ${loadaddr} ${filesize}; " \
+			"reset; " \
+		"else " \
+			"echo \"Failed to load firmware file from USB 0\"; " \
+		"fi;\0" \
+
+#define CONFIG_BOOTCOMMAND \
+	"run mmcbootcmd;"
+
+/* Link Definitions */
+#define CONFIG_LOADADDR			0x40480000
+
+#define CONFIG_SYS_LOAD_ADDR           CONFIG_LOADADDR
+
+#define CONFIG_SYS_INIT_RAM_ADDR        0x40000000
+#define CONFIG_SYS_INIT_RAM_SIZE        0x80000
+#define CONFIG_SYS_INIT_SP_OFFSET \
+        (CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
+#define CONFIG_SYS_INIT_SP_ADDR \
+        (CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
+
+#define CONFIG_ENV_OVERWRITE
+#define CONFIG_ENV_OFFSET               (64 * SZ_64K)
+#define CONFIG_ENV_SIZE			0x1000
+#define CONFIG_SYS_MMC_ENV_DEV		1   /* eMMC */
+
+/* Size of malloc() pool */
+#define CONFIG_SYS_MALLOC_LEN		((CONFIG_ENV_SIZE + (2*1024) + (16*1024)) * 1024)
+
+#define CONFIG_SYS_SDRAM_BASE           0x40000000
+#define PHYS_SDRAM                      0x40000000
+#define DEFAULT_DRAM_SIZE_MB            512
+#define CONFIG_NR_DRAM_BANKS            1
+
+#define CONFIG_SYS_MEMTEST_START	PHYS_SDRAM
+#define CONFIG_SYS_MEMTEST_END		(CONFIG_SYS_MEMTEST_START + ((256 * SZ_1M)))
+
+#define CONFIG_BAUDRATE			115200
+
+#define CONFIG_MXC_UART
+#define CONFIG_MXC_UART_BASE		UART1_BASE_ADDR
+
+/* Monitor Command Prompt */
+#undef CONFIG_SYS_PROMPT
+#define CONFIG_SYS_PROMPT		"u-boot=> "
+#define CONFIG_SYS_PROMPT_HUSH_PS2     "> "
+#define CONFIG_SYS_CBSIZE              2048
+#define CONFIG_SYS_MAXARGS             64
+#define CONFIG_SYS_BARGSIZE CONFIG_SYS_CBSIZE
+#define CONFIG_SYS_PBSIZE		(CONFIG_SYS_CBSIZE + \
+					sizeof(CONFIG_SYS_PROMPT) + 16)
+
+#define CONFIG_IMX_BOOTAUX
+
+/* USDHC */
+#define CONFIG_CMD_MMC
+#define CONFIG_FSL_ESDHC
+#define CONFIG_FSL_USDHC
+
+#define CONFIG_SYS_FSL_USDHC_NUM	2
+#define CONFIG_SYS_FSL_ESDHC_ADDR       0
+
+#define CONFIG_SUPPORT_EMMC_BOOT	/* eMMC specific */
+#define CONFIG_MMCROOT			"/dev/mmcblk2p1"  /* eMMC : partition 1 */
+#define CONFIG_SYS_MMC_IMG_LOAD_PART	1
+
+#define CONFIG_MXC_GPIO
+
+#define CONFIG_MXC_OCOTP
+#define CONFIG_CMD_FUSE
+
+#ifndef CONFIG_DM_I2C
+#define CONFIG_SYS_I2C
+#endif
+#define CONFIG_SYS_I2C_MXC_I2C1		/* enable I2C bus 1 */
+#define CONFIG_SYS_I2C_MXC_I2C2		/* enable I2C bus 2 */
+#define CONFIG_SYS_I2C_MXC_I2C3		/* enable I2C bus 3 */
+#define CONFIG_SYS_I2C_SPEED		100000
+
+/* USB configs */
+#ifndef CONFIG_SPL_BUILD
+#define CONFIG_CMD_USB
+#define CONFIG_USB_STORAGE
+#define CONFIG_USBD_HS
+#endif /* !CONFIG_SPL_BUILD */
+
+#define CONFIG_USB_GADGET_DUALSPEED
+#define CONFIG_USB_GADGET_VBUS_DRAW 2
+
+#define CONFIG_CI_UDC
+
+#define CONFIG_MXC_USB_PORTSC  (PORT_PTS_UTMI | PORT_PTS_PTW)
+#define CONFIG_USB_MAX_CONTROLLER_COUNT         2
+
+#ifdef CONFIG_VIDEO
+#define CONFIG_VIDEO_MXS
+#define CONFIG_VIDEO_LOGO
+#define CONFIG_SPLASH_SCREEN
+#define CONFIG_SPLASH_SCREEN_ALIGN
+#define CONFIG_CMD_BMP
+#define CONFIG_BMP_16BPP
+#define CONFIG_VIDEO_BMP_RLE8
+#define CONFIG_VIDEO_BMP_LOGO
+#define CONFIG_IMX_VIDEO_SKIP
+#define CONFIG_RM67191
+#endif
+
+#define CONFIG_OF_SYSTEM_SETUP
+
+#endif
